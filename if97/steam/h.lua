12cfa82c82c8833701b53d88ca0b0aff357e081d
Maxim <i@maxim-sadovskiy.ru>
2026-06-10 09:30:25 +0300

    -

--- if97/steam/h.lua
+++ if97/steam/h.lua
@@ -6,7 +6,6 @@ local minT = require 'if97.consts'.minT
 
 local steam_s_tp = require 'if97.steam.s'.t_p
 local water_s_tp = require 'if97.water.s'.t_p
-local sTP = require 'if97.steam.s'.t_p
 local water_h_tp = require 'if97.water.h'.t_p
 local saturationTemp_p = require 'if97.bounds'.saturationTemp_p
 
@@ -21,13 +20,24 @@ function h.t_p(t, p)
 end
 
 function h.p_s(p, s)
-    local t = saturationTemp_p(p)
-    local function binary_search(maxT, minT)
+    local t_sat = saturationTemp_p(p)
+
+    local function binary_search(maxT, minT, region)
         local tR
         local sR
-        for _ = 1, 20 do
+        for _ = 1, 50 do
             tR = (maxT + minT) / 2
-            sR = steam_s_tp(tR, p)
+            -- Выбираем функцию в зависимости от региона
+            if region == 'water' then
+                sR = water_s_tp(tR, p)
+            elseif region == 'mix' then
+                -- Для влажного пара используем энтропию насыщенного пара
+                sR = steam_s_tp(tR, p)
+            elseif region == 'steam' then
+                sR = steam_s_tp(tR, p)
+            end
+
+            print('не ориг', sR)
             if sR > s then
                 maxT = tR
             else
@@ -36,22 +46,31 @@ function h.p_s(p, s)
         end
         return h.t_p(tR, p)
     end
-    if t then
-        local s1 = water_s_tp(t, p)
-        if s1 < s then
-            local s2 = steam_s_tp(t, p)
-            if s2 > s then
-                local h1 = h.t_p(t, p)
-                local h2 = water_h_tp(t, p)
-                return (s - s1) * (h2 - h1) / (s2 - s1) + h1
-            else
-                return binary_search(maxT, t)
-            end
-        else
-            return binary_search(t, minT)
+
+    if t_sat then
+        local s_f = water_s_tp(t_sat, p)
+        local s_g = steam_s_tp(t_sat, p)
+
+        if s_f < s and s < s_g then
+            -- Регион 4: влажный пар (интерполяция)
+            print('тут 1 - влажный пар')
+            local h_f = water_h_tp(t_sat, p)
+            local h_g = h.t_p(t_sat, p)
+            local x = (s - s_f) / (s_g - s_f)
+            return h_f + x * (h_g - h_f)
+        elseif s >= s_g then
+            -- Регион 3: перегретый пар
+            print('тут 2 - перегретый пар')
+            return binary_search(maxT, t_sat, 'steam')
+        else -- s <= s_f
+            -- Регион 1: вода
+            print('тут 3 - вода')
+            return binary_search(t_sat, minT, 'water')
         end
     else
-        return binary_search(maxT, minT)
+        -- Давление выше критического
+        print('тут 4 - сверхкритический')
+        return binary_search(maxT, minT, 'fluid')
     end
 end
 
